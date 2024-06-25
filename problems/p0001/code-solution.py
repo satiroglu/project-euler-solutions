2c610fc14735a332ab0d5e6fe4627cb6d0a0d68d
Ahmet Erturk <aesatiroglu@gmail.com>
2024-06-25 16:20:29 +0300

    p0001 updated

--- problems/p0001/code-solution.py
+++ problems/p0001/code-solution.py
@@ -1,7 +1,10 @@
-solution = 0
+def sum_of_multiples(limit):
+    total_sum = 0
+    for i in range(limit):
+        if i % 3 == 0 or i % 5 == 0:
+            total_sum += i
+    return total_sum
 
-for i in range(1000):
-    if i % 3 == 0 or i % 5 == 0:
-        solution += i
 
-print(solution)
+# Set the limit to 1000 and print the solution
+print("The sum of all the multiples of 3 or 5 below 1000 is:", sum_of_multiples(1000))
